2156e534e7cff0723909655d22be53c5f9000bf3
Verena Lechner <v.m.s.lechner@gmail.com>
2023-04-01 18:38:46 +0200

    Initial Commit

---
+++ EisbachPegel/EisbachPegelApp.swift
@@ -0,0 +1,17 @@
+//
+//  EisbachPegelApp.swift
+//  EisbachPegel
+//
+//  Created by Verena Lechner on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct EisbachPegelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
